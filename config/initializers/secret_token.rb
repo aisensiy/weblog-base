1b3995d62a27dc66c23772fd32fe65a6ea6adfaa
aisensiy <aisensiy@gmail.com>
2013-03-26 10:35:41 +0800

    add devise and twitter-bootstrap-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewWeblogBase::Application.config.secret_token = '9524835208ec7d360a670d888e03c1781919d0a151f33b2138b3bdb774e1413d3f57b4c001677a46a074b613776b60fe3a885890cee41b2410fafcb52ff35c9f'
